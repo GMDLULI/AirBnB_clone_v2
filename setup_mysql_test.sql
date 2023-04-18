bd05a00e237c2b3b7eecd80bf6ed3e13e5ea3c38
GMDLULI <gugulethumdluli98@gmail.com>
2023-04-18 23:37:11 +0000

    preparing MySQL server for hbnb_test_db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,17 @@
+-- This script prepares a MySQL server and should:
+-- create project testing for database: hbnb_test_db
+-- create new user: hbnb_test with all privileges on the hbnb_test_db database
+-- if the user doesnt exist it should have password : hbnb_test_pwd when created
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
+-- granting all priviledges to the user: hbnb_test_db
+
+--create database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY hbnb_test_pwd;
+-- granting privileges
+GRANT SELECT ON performance_schema.* to 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
