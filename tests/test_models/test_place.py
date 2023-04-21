5afd54462717962d85e0f0b77341333966a615fa
GMDLULI <gugulethumdluli98@gmail.com>
2023-04-21 02:08:47 +0000

    testing vity

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -31,62 +31,62 @@ class test_Place(test_basemodel):
     def test_name(self):
         """testing place name attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), str if
+        self.assertEqual(type(new.name), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_description(self):
         """testing place description attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), str if
+        self.assertEqual(type(new.description), str if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_number_rooms(self):
         """testing place number of rooms attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), int if
+        self.assertEqual(type(new.number_rooms), int if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_number_bathrooms(self):
         """testing place number of bathrooms attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), int if
+        self.assertEqual(type(new.number_bathrooms), int if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_max_guest(self):
         """testing place max guest attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), int if
+        self.assertEqual(type(new.max_guest), int if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_price_by_night(self):
         """testing place price by night attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), int if
+        self.assertEqual(type(new.price_by_night), int if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_latitude(self):
         """testing place latitude attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), in if
+        self.assertEqual(type(new.latitude), in if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_longitude(self):
         """ testing place logitude attr"""
         new = self.value()
-        self.assertEqual(type(new.user_id), float if
+        self.assertEqual(type(new.longitude), float if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
 
     def test_amenity_ids(self):
         """testing place amenity_id attr """
         new = self.value()
-        self.assertEqual(type(new.user_id), list if
+        self.assertEqual(type(new.amenity_ids), list if
                          os.getenv('HBNB_TYPE_STORAGE') != 'db' else
                          type(None))
